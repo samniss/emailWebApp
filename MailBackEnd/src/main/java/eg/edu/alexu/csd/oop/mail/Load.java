9454bb4404ac045c767e2fd95e0a3c56f6d4a674
Momen Ibrahim Fawzy <58600213+Momen-Ibrahim-Fawzy@users.noreply.github.com>
2020-12-26 13:14:09 +0200

    Update Load.java

--- MailBackEnd/src/main/java/eg/edu/alexu/csd/oop/mail/Load.java
+++ MailBackEnd/src/main/java/eg/edu/alexu/csd/oop/mail/Load.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 
 public class Load {
     public User loadUserInfo(String userEmailAddress){
@@ -49,6 +50,53 @@ public class Load {
             }
         }
     return emails;
+    }
+
+    public List<File> loadFolders(String userEmailAddress,int pageNumber){//Loads  emails from a specific folder
+        String path="System\\"+userEmailAddress+"\\";
+        List<String> folderNames=loadFoldersNames(userEmailAddress);//This list contains the names of folders except the default folders
+        List<File> folders=new ArrayList<File>();
+        int start=(pageNumber-1)*10;
+        int end=(pageNumber*10)-1;
+        if(start>folderNames.size()-1){
+            return null;//Page is empty
+        }
+        else if (start<=folderNames.size()-1 &&end>folderNames.size()-1){
+            end=folderNames.size()-1;
+        }
+        for(int i=start;i<=end;i++){//Load emails
+            folders.add(new File(path+folderNames.get(i)));
+        }
+        return folders;
+    }
+
+    public List<String> loadFoldersNames(String userEmailAddress){
+        List<String> folderNames=new ArrayList<String>();
+        String path="System\\"+userEmailAddress;
+        File[] directories = new File(path).listFiles(File::isDirectory);
+        for (int i=0;i<directories.length;i++){
+            if (directories[i].getName()!="Contact"&&directories[i].getName()!="Draft"&&directories[i].getName()!="inbox"&&directories[i].getName()!="Sent"&&directories[i].getName()!="Trash"&&directories[i].isDirectory()){
+                folderNames.add(directories[i].getName());
+            }
+        }
+        return folderNames;
+    }
+
+    public boolean checkExistFolderInUserFolder(String userEmailAddress,String folderName) {
+        File system=new File("System");
+        system.mkdir();
+        if(userEmailAddress==null)
+            return false;
+        for(File f:system.listFiles()) {
+            if(f.getName().toLowerCase(Locale.ROOT).equals((userEmailAddress.toLowerCase(Locale.ROOT)))){
+                for(File inF:f.listFiles()) {
+                    if (inF.getName().toLowerCase(Locale.ROOT).equals((folderName.toLowerCase(Locale.ROOT)))) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     public List<String> loadEmailNames(String userEmailAddress,String folderName){
